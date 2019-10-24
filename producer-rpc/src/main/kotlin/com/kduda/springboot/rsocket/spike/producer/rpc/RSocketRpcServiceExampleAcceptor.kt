40b8bfde5bc3149af11ca22d67d63c01bf5f4320
Kamil Duda <kamilduda01@gmail.com>
2019-10-24 21:26:52 +0200

    Add RPC RSocket Acceptor (Work in progress)

--- producer-rpc/src/main/kotlin/com/kduda/springboot/rsocket/spike/producer/rpc/RSocketRpcServiceExampleAcceptor.kt
+++ producer-rpc/src/main/kotlin/com/kduda/springboot/rsocket/spike/producer/rpc/RSocketRpcServiceExampleAcceptor.kt
@@ -1,11 +1,9 @@
 package com.kduda.springboot.rsocket.spike.producer.rpc
 
 import com.google.protobuf.Empty
-import com.google.protobuf.Timestamp
 import com.kduda.springboot.rsocket.spike.common.rpc.*
+import com.kduda.springboot.rsocket.spike.common.rpc.InstantProtobufExtensions.toProto
 import io.netty.buffer.ByteBuf
-import io.reactivex.Flowable
-import io.reactivex.Single
 import mu.KotlinLogging
 import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
@@ -16,122 +14,69 @@ import java.util.*
 
 
 // TODO: check toString implementations
-// TODO: Use Reactor or `java.util.concurrent.Flow` (even better!) for generated code
+// TODO: tweak log messages to be the same as in RSocketProducerController
 internal class RSocketRpcServiceExampleAcceptor : RSocketRpcServiceExample {
     private val logger = KotlinLogging.logger {}
 
-    override fun requestResponse(message: RSocketRpcRequest, metadata: ByteBuf): Single<RSocketRpcResponse> {
+    override fun requestResponse(message: RSocketRpcRequest, metadata: ByteBuf): Mono<RSocketRpcResponse> {
         val messageId = message.uuid
-        logger.info("Responding to request with single response id ${UUID.fromString(messageId.value)}")
+        logger.info { "Responding to request with single response id ${UUID.fromString(messageId.value)}" }
 
-        val now = Instant.now()
-            .let {
-                Timestamp.newBuilder()
-                    .setSeconds(it.epochSecond)
-                    .setNanos(it.nano)
-                    .build()
-            }
-
-
-        val response = RSocketRpcResponse.newBuilder()
-            .apply {
-                uuid = messageId
-                time = now
-                value = "Request - response"
-            }
-            .build()
-
-        val monoResponse = Mono.just(response)
-            .doOnNext { logger.info("Responding to request with single response $it") }
-
-        return Single.fromPublisher(monoResponse)
+        return RSocketRpcResponseDto(UUID.fromString(messageId.value), Instant.now(), "Request - response")
+            .toProto()
+            .let { Mono.just(it) }
+            .doOnNext { logger.info { "Responding to request with single response $it" } }
     }
 
-    override fun requestStream(message: RSocketRpcRequest, metadata: ByteBuf): Flowable<RSocketRpcResponse> {
+    override fun requestStream(message: RSocketRpcRequest, metadata: ByteBuf): Flux<RSocketRpcResponse> {
         val messageId = message.uuid
-        logger.info("Responding to request with stream id $messageId")
+        logger.info { "Responding to request with stream id $messageId" }
 
-        val fluxResponse = Flux.interval(Duration.ofSeconds(1))
+        return Flux.interval(Duration.ofSeconds(1))
             .map {
-                val now = Instant.now()
-                    .let {
-                        Timestamp.newBuilder()
-                            .setSeconds(it.epochSecond)
-                            .setNanos(it.nano)
-                            .build()
-                    }
-
-                RSocketRpcResponse.newBuilder()
-                    .apply {
-                        uuid = messageId
-                        time = now
-                        value = "Request - stream"
-                    }.build()
-            }.log()
-            .doOnNext { logger.info("Sending streaming response $it") }
-
-        return Flowable.fromPublisher(fluxResponse)
+                RSocketRpcResponseDto(UUID.fromString(messageId.value), Instant.now(), "Request - stream")
+                    .toProto()
+            }.doOnNext { logger.info { "Sending streaming response $it" } }
     }
 
-    override fun fireAndForget(message: RSocketRpcRequest, metadata: ByteBuf): Single<Empty> {
+    override fun fireAndForget(message: RSocketRpcRequest, metadata: ByteBuf): Mono<Empty> {
         val messageId = message.uuid
 
-        logger.info("Received fire and forget data with id $messageId")
-        val monoResponse = Mono.empty<Empty>()
+        logger.info { "Received fire and forget data with id $messageId" }
 
-        return Single.fromPublisher(monoResponse)
+        return Mono.empty<Empty>()
     }
 
     override fun streamStream(
         message: Publisher<RSocketRpcStreamingRequest>,
         metadata: ByteBuf
-    ): Flowable<RSocketRpcStreamingResponse> {
-        val fluxInput = Flux.from(message)
-
-        val fluxResponse = fluxInput.flatMap {
-            logger.info("Responding with stream ${it.streamId}")
+    ): Flux<RSocketRpcStreamingResponse> {
+        return Flux.from(message).flatMap {
+            logger.info { "Responding with stream ${it.streamId}" }
 
             val messageId = it.uuid
             val streamId = it.streamId
 
             Flux.interval(Duration.ofSeconds(1))
                 .map {
-                    val now = Instant.now()
-                        .let {
-                            Timestamp.newBuilder()
-                                .setSeconds(it.epochSecond)
-                                .setNanos(it.nano)
-                                .build()
-                        }
-
-                    RSocketRpcResponse.newBuilder()
-                        .apply {
-                            uuid = messageId
-                            time = now
-                            value = "Request - stream"
-                        }.build()
+                    RSocketRpcResponseDto(UUID.fromString(messageId.value), Instant.now(), "Request - stream")
                 }
-                .map {
-                    RSocketRpcStreamingResponse.newBuilder()
-                        .setResponse(it)
-                        .setStreamId(streamId)
-                        .build()
-                }.log()
-        }.doOnNext { logger.info("Sending streaming response $it") }
-
-        return Flowable.fromPublisher(fluxResponse)
+                .map { RSocketRpcStreamingResponseDto(streamId, it).toProto() }
+        }.doOnNext { logger.info { "Sending streaming response $it" } }
     }
 
-    override fun exception(message: RSocketRpcRequest, metadata: ByteBuf): Single<RSocketRpcResponse> {
+    override fun exception(message: RSocketRpcRequest, metadata: ByteBuf): Mono<RSocketRpcResponse> {
         throw IllegalStateException("Should by handled by exception handler for this specific type")
-            .also { logger.error("Throwing exception", it) }
+            .also { logger.error(it) { "Throwing exception" } }
     }
 
 }
 /*
     @MessageExceptionHandler
     internal fun illegalStateExceptionHandler(exception: IllegalStateException) =
-        exception.also { logger.info("Handling exception $it") }
+        exception.also { logger.info{"Handling exception $it"} }
             .let { messageFactory.build(UUID.randomUUID(), exception.message ?: "Fallback message") }
             .let { Mono.just(it) }
  */
+
+
